c7bf21c40f6825e34a90504d19e81d409cc17ce8
Kevin Luo <kevin.luo@outlook.com>
2013-12-31 20:54:40 +0800

    update with Data Part II

--- kluo/Controllers/ReviewsController.cs
+++ kluo/Controllers/ReviewsController.cs
@@ -1,140 +1,36 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Web;
-//using System.Web.Mvc;
-//using kluo.Models;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using kluo.Models;
 
-//namespace kluo.Controllers
-//{
-//    public class ReviewsController : Controller
-//    {
-//        //
-//        // GET: /Reviews/
+namespace kluo.Controllers
+{
+    public class ReviewsController : Controller
+    {
+        //
+        // GET: /Reviews/
 
-//        public ActionResult Index()
-//        {
-//            var model =
-//                from r in _reviews
-//                orderby r.Country
-//                select r;
+        kluoDb _db = new kluoDb();
 
+        public ActionResult Index([Bind(Prefix = "id")] int restaurantId)
+        {
+            var restaurant = _db.Restaurants.Find(restaurantId);
+            if (restaurant != null)
+            {
+                return View(restaurant);
+            }
+            return HttpNotFound();
+        }
 
-//            return View(model);
-//        }
-
-//        //
-//        // GET: /Reviews/Details/5
-
-//        public ActionResult Details(int id)
-//        {
-//            return View();
-//        }
-
-//        //
-//        // GET: /Reviews/Create
-
-//        public ActionResult Create()
-//        {
-//            return View();
-//        }
-
-//        //
-//        // POST: /Reviews/Create
-
-//        [HttpPost]
-//        public ActionResult Create(FormCollection collection)
-//        {
-//            try
-//            {
-//                // TODO: Add insert logic here
-
-//                return RedirectToAction("Index");
-//            }
-//            catch
-//            {
-//                return View();
-//            }
-//        }
-
-//        //
-//        // GET: /Reviews/Edit/5
-
-//        public ActionResult Edit(int id)
-//        {
-//            return View();
-//        }
-
-//        //
-//        // POST: /Reviews/Edit/5
-
-//        [HttpPost]
-//        public ActionResult Edit(int id, FormCollection collection)
-//        {
-//            try
-//            {
-//                // TODO: Add update logic here
-
-//                return RedirectToAction("Index");
-//            }
-//            catch
-//            {
-//                return View();
-//            }
-//        }
-
-//        //
-//        // GET: /Reviews/Delete/5
-
-//        public ActionResult Delete(int id)
-//        {
-//            return View();
-//        }
-
-//        //
-//        // POST: /Reviews/Delete/5
-
-//        [HttpPost]
-//        public ActionResult Delete(int id, FormCollection collection)
-//        {
-//            try
-//            {
-//                // TODO: Add delete logic here
-
-//                return RedirectToAction("Index");
-//            }
-//            catch
-//            {
-//                return View();
-//            }
-//        }
-
-//        private static List<RestaurantReview> _reviews = new List<RestaurantReview>
-//        {
-//            new RestaurantReview
-//                {
-//                    Id = 1,
-//                    Name = "Cinnamon Club",
-//                    City = "London",
-//                    Country = "UK",
-//                    Rating = 10
-//                },
-//            new RestaurantReview
-//                {
-//                    Id = 2,
-//                    Name = "Marrakesh",
-//                    City = "D.C",
-//                    Country = "USA",
-//                    Rating = 10
-//                },
-//            new RestaurantReview
-//                {
-//                    Id = 3,
-//                    Name = "The House of Elliot",
-//                    City = "Ghent",
-//                    Country = "Belgium",
-//                    Rating = 10
-//                }
-//        };
-//    }
-//}
+        protected override void Dispose(bool disposing)
+        {
+            if (_db != null)
+            {
+                _db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
